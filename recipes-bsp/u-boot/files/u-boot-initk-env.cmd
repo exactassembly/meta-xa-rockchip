c8ac724ee40ec55f75d697dfe9ada1a86c8cb8e4
JeremiahD <jdurham@xassembly.com>
2024-05-03 21:40:19 +0000

    convert to FIT images

--- recipes-bsp/u-boot/files/u-boot-initk-env.cmd
+++ recipes-bsp/u-boot/files/u-boot-initk-env.cmd
@@ -4,12 +4,12 @@ board=evb_px30
 board_name=evb_px30
 bootdelay=2
 cpu=armv8
-bootimg=Image
+bootimg=fitImage
 fdt_addr_r=0x08300000
 loadaddr=0x800800
 fdtimage=px30-evb.dtb
 bootcmd=run mmcload; run mmcboot
 mmcbootpart=6
 mmcloadcmd=fatload
-mmcload=mmc rescan;${mmcloadcmd} mmc 0:${mmcbootpart} ${loadaddr} ${bootimg};${mmcloadcmd} mmc 0:${mmcbootpart} ${fdt_addr_r} ${fdtimage}
-mmcboot=setenv bootargs "earlycon=uart8250,mmio32,0xff160000 swiotlb=1 console=ttyS2,115200n8 rw root=/dev/mmcblk2p7 rootfstype=ext4 rootwait"; booti ${loadaddr} - ${fdt_addr_r}
+mmcload=mmc rescan;${mmcloadcmd} mmc 0:${mmcbootpart} ${loadaddr} ${bootimg}
+mmcboot=setenv bootargs "earlycon=uart8250,mmio32,0xff160000 swiotlb=1 console=ttyS2,115200n8 rw root=/dev/mmcblk2p7 rootfstype=ext4 rootwait"; bootm ${loadaddr}
--- recipes-bsp/u-boot/u-boot%.bbappend
+++ recipes-bsp/u-boot/u-boot%.bbappend
@@ -8,9 +8,9 @@ SRC_URI += "file://01_use_default_env.cfg"
 
 DEPENDS:append = " xxd-native"
 
-do_configure:prepend:px30() {
-    sed -i 's/\(fdtimage=\).*/\1${RK_KERNEL_DEVICETREE}/' ${S}/../u-boot-initk-env.cmd
-}
+#do_configure:prepend:px30() {
+#    sed -i 's/\(fdtimage=\).*/\1${RK_KERNEL_DEVICETREE}/' ${S}/../u-boot-initk-env.cmd
+#}
 
 do_deploy:append:px30() {
     install -D -m 644 ${B}/spl/u-boot-spl.bin ${DEPLOYDIR}/u-boot-spl-${MACHINE}-${PV}-${PR}.bin
